c07ab91c04539a4264041ea8f294b12bbdb9ddfd
JustSenoko <just.senoko@gmail.com>
2019-04-05 21:11:22 +0300

    требуемый размер массива вынесен в поле класса в вызов конструктора исключения MyArraySizeException добавлен тип исходного исключения

--- src/ru/geekbrains/lesson2/Application.java
+++ src/ru/geekbrains/lesson2/Application.java
@@ -2,11 +2,24 @@ package ru.geekbrains.lesson2;
 
 public class Application {
 
+    static final int SIZE = 4;
+
     public static void main(String[] args) {
         String[][][] arrays = new String[3][][];
-        String[][] arr1 = {{"1", "2", "3", "4"}, {"2", "3", "4", "5"},{"3", "4", "5", "6"},{"4", "5", "6", "7"}};
-        String[][] arr2 = {{"1", "b", "c", "d"}, {"2", "3", "4", "5"},{"3", "4", "5", "6"},{"4", "5", "6", "7"}};
-        String[][] arr3 = {{"1", "2", "3", "4", "5"}, {"2", "3", "4", "5", "6"},{"3", "4", "5", "6"},{"4", "5", "6", "7"}};
+        String[][] arr1 =  {{"1", "2", "3", "4"},
+                            {"2", "3", "4", "5"},
+                            {"3", "4", "5", "6"},
+                            {"4", "5", "6", "7"}};
+
+        String[][] arr2 =  {{"1", "b", "c", "d"},
+                            {"2", "3", "4", "5"},
+                            {"3", "4", "5", "6"},
+                            {"4", "5", "6", "7"}};
+
+        String[][] arr3 =  {{"1", "2", "3", "4", "5"},
+                            {"2", "3", "4", "5", "6"},
+                            {"3", "4", "5", "6"},
+                            {"4", "5", "6", "7"}};
         arrays[0] = arr1;
         arrays[1] = arr2;
         arrays[2] = arr3;
@@ -15,30 +28,28 @@ public class Application {
             printArray(arr);
             try {
                 System.out.printf("Сумма элементов массива равна %s%n%n", sumArray(arr));
-            } catch (MyArraySizeException ex) {
-                System.out.println(ex);
-            } catch (MyArrayDataException ex) {
+            } catch (MyArraySizeException | MyArrayDataException ex) {
                 System.out.println(ex);
             }
         }
     }
 
     private static int sumArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
-        int requiredSize = 4;
-        if (arr.length != requiredSize) {
-            throw new MyArraySizeException(requiredSize);
+
+        if (arr.length != SIZE) {
+            throw new MyArraySizeException(SIZE);
         }
 
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].length != 4) {
-                throw new MyArraySizeException(requiredSize);
+                throw new MyArraySizeException(SIZE);
             }
             for (int j = 0; j < arr[i].length; j++) {
                 try {
                     sum += Integer.parseInt(arr[i][j]);
                 } catch (NumberFormatException ex) {
-                    throw new MyArrayDataException(i, j);
+                    throw new MyArrayDataException(i, j, ex);
                 }
             }
         }
